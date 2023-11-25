5339a3b88f1632c61f036ce48a949e9f0bce35c0
Benjamin Eder <ben-eder@freenet.de>
2023-11-25 19:33:27 +0100

    feat: customize layout #1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
   import "../app.postcss";
-  import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+  import { AppShell, AppBar, LightSwitch } from "@skeletonlabs/skeleton";
+
+  const title = "DeltaCount";
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
 
 <!-- App Shell -->
 <AppShell>
@@ -9,32 +15,30 @@
     <!-- App Bar -->
     <AppBar>
       <svelte:fragment slot="lead">
-        <strong class="text-xl uppercase">Skeleton</strong>
+        <strong class="text-xl uppercase">{title}</strong>
       </svelte:fragment>
       <svelte:fragment slot="trail">
+        <LightSwitch />
         <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://discord.gg/EXqV7W8MtY"
+          class="btn btn-icon variant-ghost-surface"
+          href="https://github.com/srtigers98/deltacount"
           target="_blank"
           rel="noreferrer"
         >
-          Discord
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://twitter.com/SkeletonUI"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Twitter
-        </a>
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://github.com/skeletonlabs/skeleton"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
+          <!-- SVG code from https://flowbite.com/icons/ -->
+          <svg
+            class="w-6 h-6 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+              clip-rule="evenodd"
+            />
+          </svg>
         </a>
       </svelte:fragment>
     </AppBar>
