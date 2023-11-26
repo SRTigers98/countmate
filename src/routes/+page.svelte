c035ed26ff2b41947237ed293e86f27f646cf77a
Benjamin Eder <ben-eder@freenet.de>
2023-11-26 12:22:54 +0100

    style: use gradient styles for landing page #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,22 @@
 </script>
 
 <section>
-  <h1 class="h1">DeltaCount</h1>
+  <h1 class="h1 mb-4">
+    <span class="title">DeltaCount</span>
+  </h1>
 
   Another counting application that stores your counters
   <strong>locally in the browser</strong>.
 </section>
 
-<a href="{base}/counter" class="btn variant-filled"> Get Started </a>
+<a href="{base}/counter" class="btn bg-gradient-to-br variant-gradient-primary-secondary">
+  Get Started
+</a>
+
+<style>
+  .title {
+    @apply bg-clip-text text-transparent box-decoration-clone;
+    @apply bg-gradient-to-br;
+    @apply from-primary-500 via-tertiary-500 to-secondary-500;
+  }
+</style>
